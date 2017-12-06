451c3e827b0ebb144fba2eb67d2a8688ed1179ef
ricanontherun@gmail.com <ricanontherun@gmail.com>
2017-12-05 21:24:56 -0500

    Added a package for decoding the POST params. Started adding the join form.

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
+app.use(bodyParser.urlencoded({extended: false}));
 
 // Create an http server which pipes to the express app.
 var server = require('http').createServer(app);
@@ -7,15 +10,26 @@ var server = require('http').createServer(app);
 // Create an socket io handler which pipes to the http server.
 var io = require('socket.io')(server);
 
+var connected_clients = {};
+
+// When a connection is made, setup some event handlers.
 io.on('connection', function(client) {
-    console.log('Client connected');
+    connected_clients[client.id] = {
+        alias: ""
+    };
 
     // Emit a 'message' event to the client.
-    client.emit("connection-registered", {hello: 'World'});
+    client.emit('connection-registered', {
+        id: client.id
+    });
 
     // Setup the server socket to listen for incoming client messages, forwarding them to the other clients.
-    client.on('message', function(message) {
-        client.broadcast.emit('message', message);
+    client.on(client.id + '-message', function(message) {
+        // Send the message to ourselves.
+        client.emit('message-relay', message);
+
+        // broadcast the message to everyone else.
+        client.broadcast.emit('message-relay', message);
     });
 });
 
@@ -23,6 +37,10 @@ io.on('connection', function(client) {
 app.get('/', function(request, response) {
     // Super userful for sending html views.
     response.sendFile(__dirname + '/views/index.html');
+});
+
+app.post('/join', function(request, response) {
+    console.log(request.body);
 });
 
 server.listen(8080);
